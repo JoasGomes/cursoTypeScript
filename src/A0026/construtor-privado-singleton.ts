5ff2c6e6dcb822ceace34074afbcccc429ec99d5
JoasGomes <jojoca.vitor777@gmail.com>
2022-05-27 04:19:43 -0300

    construtor privado/singleton

---
+++ src/A0026/construtor-privado-singleton.ts
@@ -0,0 +1,26 @@
+//singleton
+export class Database {
+  private static database: Database;
+
+  private constructor(
+    private host: string,
+    private user: string,
+    private password: string,
+  ) {}
+
+  connect(): void {
+    console.log(`conectado: ${this.host}, ${this.user}, ${this.password}`);
+  }
+
+  static getDatabase(host: string, user: string, password: string): Database {
+    if (Database.database) return Database.database;
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  }
+}
+
+const db1 = Database.getDatabase('localhost', 'root', '123456');
+db1.connect();
+
+const db2 = Database.getDatabase('localhost', 'root', '123456');
+db2.connect();
