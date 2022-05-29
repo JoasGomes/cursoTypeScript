7673e80361db47ec781e1205052401c57f7011e7
JoasGomes <jojoca.vitor777@gmail.com>
2022-05-29 18:56:51 -0300

    interface

---
+++ src/A0032/interface.ts
@@ -0,0 +1,35 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto(): string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa2 {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto() {
+    return this.nome + ' ' + this.sobrenome;
+  },
+  nome: 'luiz',
+  sobrenome: 'andre',
+};
+
+const pessoa = new Pessoa('JOAS', 'VITOR');
+console.log(pessoa);
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
